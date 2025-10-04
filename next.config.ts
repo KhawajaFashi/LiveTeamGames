b590cce420236034ab3958b71d43cded431982eb
Khawaja Fashi Ud Din Abdullah <khawajafashi449623@gmail.com>
2025-10-04 16:22:33 +0500

    Fixing Deployment Issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizeCss: false, // disable lightningcss, use postcss instead
+  },
 };
 
 export default nextConfig;
